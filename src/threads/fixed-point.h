4e51843ca195646167691fc4e273f3125f796c82
Urjit Sharma <104162053+UrjitS@users.noreply.github.com>
2023-11-18 14:44:13 -0800

    Fixed calculation

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,36 +1,34 @@
 #ifndef THREADS_FIXED_POINT_H
 #define THREADS_FIXED_POINT_H
 
-#include <stdint.h>
+typedef int fixed_point;
 
-#define FIXED_POINT_FACTOR 16384
+#define FRACTION_BITS 15
 
-#define convert_int_to_fixed_point(n) (n * FIXED_POINT_FACTOR)
-#define convert_fixed_point_to_int_round_towards_zero(x) (x / FIXED_POINT_FACTOR)
-#define convert_fixed_point_to_int_round_to_nearest(x) (x >= 0 ? (x + FIXED_POINT_FACTOR / 2) / FIXED_POINT_FACTOR : (x - FIXED_POINT_FACTOR / 2) / FIXED_POINT_FACTOR)
+#define convert_int_to_fixed_point(int_value) ((fixed_point)(int_value << FRACTION_BITS))
+
+#define convert_fixed_point_to_int_round_towards_zero(fixed_point_value) (fixed_point_value >> FRACTION_BITS)
+
+#define convert_fixed_point_to_int_round_to_nearest(fixed_point_value) (fixed_point_value >= 0 ? ((fixed_point_value + (1 << (FRACTION_BITS - 1))) >> FRACTION_BITS) : ((fixed_point_value - (1 << (FRACTION_BITS - 1))) >> FRACTION_BITS))
 
 /* Add two fixed-point numbers */
 #define add_fixed_point_numbers(fixed_point_number1, fixed_point_number2) (fixed_point_number1 + fixed_point_number2)
+/* Add integer n to fixed-point number x */
+#define add_int_to_fixed_point_number(fixed_point_number, int_value) (fixed_point_number + (int_value << FRACTION_BITS))
 
 /* Subtract two fixed-point numbers */
 #define subtract_fixed_point_numbers(fixed_point_number1, fixed_point_number2) (fixed_point_number1 - fixed_point_number2)
-
-/* Add integer n to fixed-point number x */
-#define add_int_to_fixed_point_number(fixed_point_number, n) (fixed_point_number + convert_int_to_fixed_point(n))
-
 /* Subtract integer n from fixed-point number x */
-#define subtract_int_from_fixed_point_number(fixed_point_number, n) (fixed_point_number - convert_int_to_fixed_point(n))
+#define subtract_int_from_fixed_point_number(fixed_point_number, int_value) (fixed_point_number - (int_value << FRACTION_BITS))
 
 /* Multiply two fixed-point numbers */
-#define multiply_fixed_point_numbers(fixed_point_number1, fixed_point_number2) ((int32_t) (((int64_t)fixed_point_number1) * fixed_point_number2 / FIXED_POINT_FACTOR))
+#define multiply_fixed_point_numbers(fixed_point_number1, fixed_point_number2) (fixed_point_number1 * fixed_point_number2)
+/* Multiply fixed-point number x by integer n */
+#define multiply_fixed_point_numbers_by_int(fixed_point_number, int_value) ((fixed_point)(((int64_t) fixed_point_number) * int_value >> FRACTION_BITS))
 
 /* Divide two fixed-point numbers */
-#define divide_fixed_point_numbers(fixed_point_number1, fixed_point_number2) ((int32_t) (((int64_t)fixed_point_number1) * FIXED_POINT_FACTOR / fixed_point_number2))
-
-/* Multiply fixed-point number x by integer n */
-#define multiply_fixed_point_number_by_int(fixed_point_number, n) (fixed_point_number * n)
-
+#define divide_fixed_point_numbers(fixed_point_number1, fixed_point_number2) (fixed_point_number1 / fixed_point_number2)
 /* Divide fixed-point number x by integer n */
-#define divide_fixed_point_number_by_int(fixed_point_number, n) (fixed_point_number / n)
+#define divide_integer_by_fixed_point(int_value, fixed_point_number) ((fixed_point)((((int64_t) int_value) << FRACTION_BITS) / fixed_point_number))
 
 #endif
